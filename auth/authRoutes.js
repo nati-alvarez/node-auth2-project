39acfed5d51a8aa66ad73251d7bd32d15d8d8c5b
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-24 02:56:48 -0400

    Created login endpoint

--- auth/authRoutes.js
+++ auth/authRoutes.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const db = require("../data/db");
 
 router.post("/login", async (req, res)=>{
-    
+    try{
+        const {username, password} = req.body;
+        if(!username || !password) res.status(400).json({message: "Username and password are required"});
+        
+        const user = await db("users").where({username: username}).first();
+        if(!user) return res.status(401).json({message: "Incorrect username or password"});
+        if(!bcrypt.compareSync(password, user.password)) return res.status(401).json({message: "Incorrect username or password"});
+        const token = generateToken(user);
+        res.status(200).json({message: "Login successful", token: token});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "A server error occurred"});
+    }
 });
 
 router.post("/signup", async (req, res)=>{
+    try{
     const {username, department, password} = req.body;
     if(!username || !department || !password) return res.status(400).json({message: "All fields are required"});
     let hash = bcrypt.hashSync(password, 14);
@@ -22,6 +36,23 @@ router.post("/signup", async (req, res)=>{
         console.log(err);
         res.status(500).json({message: "A server error occurred"});
     });
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: "A server error occurred"});
+    }
 });
+
+function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+
+    const options = {
+        expiresIn: "1d"
+    }
+
+    return jwt.sign(payload, "SDFAWAISEFJD0SJFNWAFAWIE09JEFSDOJFS0", options);
+}
 
 module.exports = router;
